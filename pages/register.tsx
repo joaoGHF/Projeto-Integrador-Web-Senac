7c3e1670c90844aba7852aa6ae5be3e6630b104a
JoaoGHF <joao.gabrielhorsth.fonseca@escola.pr.gov.br>
2024-08-15 15:53:37 -0300

    Aula do dia 18/28/2024

--- pages/register.tsx
+++ pages/register.tsx
@@ -4,14 +4,20 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Register() {
   return (
     <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`} >
       <Head>
-        <title>FindMyGame | Home</title>
+        <title>FindMyGame | Cadastro</title>
       </Head>
 
-      
+      <div>
+        <h1>FindMyGame — Register</h1>
+
+        <form>
+
+        </form>
+      </div>
     </main>
   );
 }
