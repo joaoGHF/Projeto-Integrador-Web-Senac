238cdc0a464f148b6743d47479c37dab6769453c
JoaoGHF <joao.gabrielhorsth.fonseca@escola.pr.gov.br>
2024-09-05 16:20:48 -0300

    Style Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,76 @@
 import Image from "next/image";
 import Head from "next/head";
-import { Inter } from "next/font/google";
 import styles from "@/styles/home.module.css";
-import { getCookie } from "cookies-next";
+import { deleteCookie, getCookie } from "cookies-next";
 import { checkToken } from "@/services/tokenConfig";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+
+  const router = useRouter();
+
+  function logout() {
+    deleteCookie('authorization');
+    router.push(`/user/login`);
+  }
+
   return (
     <main id={styles.main} className={`flex min-h-screen flex-col`} >
       <Head>
         <title>FindMyGame | Home</title>
       </Head>
 
-      
+      {/* Barra superior de navegação */}
+      <nav className={styles.topNav}>
+      <Link href={`/`}><Image src="/logo.png" alt="Logo" width="100" height="100" className={styles.logoImg} /></Link>
+
+        <div className={styles.searchContainer}>
+          <input type="text" className={styles.searchBar} />
+          <button className={styles.send}>Enviar</button>
+        </div>
+
+        <Link href={`/game/create`} className={styles.createMovie}>Criar Jogo</Link>
+
+        <button className={styles.logoutBtn} onClick={logout}>Logout</button>
+
+      </nav>
+
+      {/* Container principal, ele vai conter o GRID */}
+      <div className={styles.mainContainer}>
+
+        {/* Painel esquerdo */}
+        <div className={styles.leftContainer}>
+
+        </div>
+
+        {/* Painel direito */}
+        <div className={styles.rightContainer}>
+
+              <div className={styles.card}>
+                <img src='' alt="Game Banner" />
+                <div className={styles.cardInfos}>
+                  <h2 className={styles.movieTitle}></h2>
+                  <p></p>
+                  <p>Gêneros do Jogo</p>
+                  <p></p>
+                </div>
+              </div>
+        
+
+        </div>
+
+      </div>
+
+
+
     </main>
   );
 }
+
+
 
 export function getServerSideProps({ req, res }: any) {
   try {
