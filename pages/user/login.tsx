ef831455e9f30e701e791f6e3af26d878e10478b
JoaoGHF <joao.gabrielhorsth.fonseca@escola.pr.gov.br>
2024-09-05 14:24:37 -0300

    Style Login

--- pages/user/login.tsx
+++ pages/user/login.tsx
@@ -9,39 +9,62 @@ import { checkToken } from "@/services/tokenConfig";
 
 export default function Login() {
   return (
-    <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`} >
+    <main id={styles.main} className={`flex min-h-screen flex-col`} >
       <Head>
         <title>FindMyGame | Login</title>
         <link rel="shortcut icon" href="/logo.png" type="image/x-icon" />
       </Head>
 
       <div className={styles.container}>
-        <nav>
-          <h1>FindMyGame</h1>
+        <nav className={styles.topNav}>
+          <Link href={`/user/login`}><Image src="/logo.png" alt="Logo" width="100" height="100" className={styles.logoImg} /></Link>
 
-          <Image src="/logo.png" alt="Logo" width="100" height="100" />
-
+          <Link href={`/user/login`}><h1 className={styles.title}><span className={styles.brand} >FindMyGame</span></h1></Link>
         </nav>
 
-        <form>
-          <h2>Bem-vindo ao <span>FindMyGame</span></h2>
+        <form className={styles.form}>
+          <h2 className={styles.welcome}>Bem-vindo ao <span className={styles.brand}>FindMyGame</span></h2>
 
-          <h2>Registrar-se</h2>
+          <h2 className={styles.formTitle}>Login</h2>
 
-          <div>
-            <p>Nome de Usuário</p>
-            <input type="text" placeholder="Nome de Usuário" /><br />
+          <div className={styles.divFormFields}>
+            <p className={styles.formLabel}>Nome de Usuário</p>
+            <input type="text" placeholder="Nome de Usuário" className={styles.inputText} /><br />
 
-            <p>Senha</p>
-            <input type="password" placeholder="Senha" /><br />
+            <p className={styles.formLabel}>Senha</p>
+            <input type="password" placeholder="Senha" className={styles.inputText} /><br />
 
 
-            <button type="submit">Login</button>
+            <button type="submit" className={styles.submitBtn}>Login</button>
           </div>
 
-          <Link href={`/user/register`}>Não tenho uma conta</Link>
+          <Link href={`/user/register`} className={styles.redirectLink}>Não tenho uma conta</Link>
         </form>
       </div>
     </main>
   );
+}
+
+export function getServerSideProps({ req, res }: any) {
+  try {
+      const token = getCookie('authorization', {req, res});
+
+      if (!token) {
+          throw new Error('Invalid Token!')
+      } else {
+          checkToken(token);
+      }
+
+      return {
+          redirect: {
+              permanent: false,
+              destination: '/'
+          }
+      };
+
+  } catch (err) {
+      return {
+          props: {}
+      }
+  }
 }
