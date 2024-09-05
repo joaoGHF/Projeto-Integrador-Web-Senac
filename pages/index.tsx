b47151f518d73a8f3275b31c26839c7e81756596
JoaoGHF <joao.gabrielhorsth.fonseca@escola.pr.gov.br>
2024-09-05 14:46:08 -0300

    Initialize Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import Head from "next/head";
 import { Inter } from "next/font/google";
+import styles from "@/styles/home.module.css";
+import { getCookie } from "cookies-next";
+import { checkToken } from "@/services/tokenConfig";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`} >
+    <main id={styles.main} className={`flex min-h-screen flex-col`} >
       <Head>
         <title>FindMyGame | Home</title>
       </Head>
@@ -14,4 +17,31 @@ export default function Home() {
       
     </main>
   );
+}
+
+export function getServerSideProps({ req, res }: any) {
+  try {
+    const token = getCookie('authorization', { req, res });
+
+    if (!token) {
+      throw new Error('Invalid Token');
+    }
+    else {
+      checkToken(token);
+    }
+
+    return {
+      props: {}
+    }
+
+  }
+  catch (err) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/user/login'
+      },
+      props: {}
+    }
+  }
 }
